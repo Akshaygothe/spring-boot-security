1d455cf81d6fed62e7d78837fee84f891d1ff78a
akshaygothe <akshaygothe56@gmail.com>
2023-08-25 19:35:33 +0530

    storing username, password and roles in database

--- src/main/java/com/luv2code/springboot/cruddemo/security/DemoSecurityConfig.java
+++ src/main/java/com/luv2code/springboot/cruddemo/security/DemoSecurityConfig.java
@@ -1,5 +1,7 @@
 package com.luv2code.springboot.cruddemo.security;
 
+import javax.sql.DataSource;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -8,11 +10,19 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class DemoSecurityConfig {
 
+	@Bean
+	public UserDetailsManager userDetailsManager(DataSource dataSource) {
+		return new JdbcUserDetailsManager(dataSource);
+	}
+	
+	/*
 	@Bean
 	public InMemoryUserDetailsManager userDetailsManager() {
 		
@@ -36,6 +46,7 @@ public class DemoSecurityConfig {
 		
 		return new InMemoryUserDetailsManager(john, mary, susan);
 	}
+	*/
 	
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
